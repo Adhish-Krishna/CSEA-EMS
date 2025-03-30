1e1e32a34eb69c5f8548fad3f9c676b4013513bb
Adhish-Krishna <adhishthesak@gmail.com>
2025-03-30 21:34:48 +0530

    added .env and .env.example

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express, {Express, Request, Response} from "express";
+import dotenv from 'dotenv';
 
-const PORT: number = 3000;
+dotenv.config()
+
+const PORT: string = process.env.PORT || '3000'
 
 const app: Express = express();
 
