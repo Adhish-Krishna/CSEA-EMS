159bae08ed8f02ab203ff9b92c4d888b225773a6
chandru262005 <tvchandru12@gmail.com>
2025-04-06 22:21:39 +0530

    included updateEvent route

--- src/api/event/types.ts
+++ src/api/event/types.ts
@@ -2,3 +2,16 @@ export interface EventRegistration {
     event_id: number,
     teamName: string
 }
+
+//interface for updating event details
+export interface UpdateEventDto {
+    name?: string;
+    about?: string;
+    date?: Date | string; 
+    event_type?: string;
+    event_category?: string;
+    min_no_member?: number;
+    max_no_member?: number;
+    venue?: string;
+  }
+  
