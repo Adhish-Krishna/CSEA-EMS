b6c72e2b66300e77e223ac6e87928ccf5a5d3d3c
DARSHAN-V-G <darshanguna22@gmail.com>
2025-08-07 13:48:29 +0530

    .

--- src/server.ts
+++ src/server.ts
@@ -37,7 +37,7 @@ const corsOptions = {
       'http://localhost:3001',    
       'http://localhost:5173',     
       'http://localhost:5174',  
-      'https://ems-frontend-pied-xi.vercel.app/',
+      'https://ems-frontend-pied-xi.vercel.app',
     ];
     
     if (allowedOrigins.indexOf(origin) !== -1) {
