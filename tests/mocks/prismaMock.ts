ea406fee65fe0654929ccb8c9887cab0ffced63b
Dhaarun <dhaarunabhimanyu@gmail.com>
2025-10-22 22:39:18 +0530

    Added global admin login tests, and create event test suites

--- tests/mocks/prismaMock.ts
+++ tests/mocks/prismaMock.ts
@@ -1,42 +1,24 @@
 type Fn = jest.Mock<any, any>;
 export interface PrismaMock {
-  users: {
-    findFirst: Fn;
-    create: Fn;
-    update: Fn;
-  };
-  emailverification: {
-    findFirst: Fn;
-    create: Fn;
-  };
-  usersecuritycode: {
-    findFirst: Fn;
-    create: Fn;
-    update: Fn;
-  };
-  clubmembers: {
-    findFirst: Fn;
-  };
+  users: { findFirst: Fn; findMany: Fn; create: Fn; update: Fn; };
+  emailverification: { findFirst: Fn; create: Fn; };
+  usersecuritycode: { findFirst: Fn; create: Fn; update: Fn; };
+  clubmembers: { findFirst: Fn; };
+  globaladmins: { findFirst: Fn; create: Fn; };
+  events: { findFirst: Fn; create: Fn; };
+  organizingclubs: { create: Fn; };
+  eventconvenors: { create: Fn; };
 }
 
 export const prismaMock: PrismaMock = {
-  users: {
-    findFirst: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-  },
-  emailverification: {
-    findFirst: jest.fn(),
-    create: jest.fn(),
-  },
-  usersecuritycode: {
-    findFirst: jest.fn(),
-    create: jest.fn(),
-    update: jest.fn(),
-  },
-  clubmembers: {
-    findFirst: jest.fn(),
-  },
+  users: { findFirst: jest.fn(), findMany: jest.fn(), create: jest.fn(), update: jest.fn() },
+  emailverification: { findFirst: jest.fn(), create: jest.fn() },
+  usersecuritycode: { findFirst: jest.fn(), create: jest.fn(), update: jest.fn() },
+  clubmembers: { findFirst: jest.fn() },
+  globaladmins: { findFirst: jest.fn(), create: jest.fn() },
+  events: { findFirst: jest.fn(), create: jest.fn() },
+  organizingclubs: { create: jest.fn() },
+  eventconvenors: { create: jest.fn() },
 };
 
 export const resetPrismaMock = () => {
